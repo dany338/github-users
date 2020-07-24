13a4855e8e83bb40ef6e918febda71291dbad04d
Daniel Andres Gallo Garcia <dgallo@teravisiontech.com>
2020-07-24 18:01:11 -0500

    Updates

---
+++ precache-manifest.5daf2a609efc3f5941160c3f93cb6bf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab5b0e1a422e5509bdbccbe64c112e5",
+    "url": "/github-users/index.html"
+  },
+  {
+    "revision": "a317fdd787f02f19aa77",
+    "url": "/github-users/static/js/2.13cc9c92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-users/static/js/2.13cc9c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "877404f68bf0eac9a157",
+    "url": "/github-users/static/js/main.303250b0.chunk.js"
+  },
+  {
+    "revision": "f453baa956893dd8728d",
+    "url": "/github-users/static/js/runtime-main.75e0f370.js"
+  }
+]);
